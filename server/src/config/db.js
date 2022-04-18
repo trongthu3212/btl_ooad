70bfa0f3196a5187d13067c751945d184adf0710
zh82 <20020157@vnu.edu.vn>
2022-04-18 22:01:00 +0700

    Add monodb connect

---
+++ server/src/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    username: 'group5_admin',
+    password: 'heJt9Gbwexzn1gDX',
+    clusterName: 'codeportalcluster',
+    databaseName: 'codeportaldb'
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,49 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose')
 
 // Application's requirements
 const appConfig = require('./config/app');
+const dbConfig = require('./config/db');
 const apiRouters = require('./routes/api');
+const userModel = require('./models/user');
 
 const app = express();
 
-app.use('/api/', apiRouters)
-app.listen(appConfig.port, () => console.log('Listening on port %d', appConfig.port))
+function sampleDbInsert() {
+  var testUser = new userModel({
+    username: 'testUser',
+    email: 'testUser@gmail.com',
+    password: 'testUserPassword'
+  });
+
+  testUser.save()
+    .then(user => {
+      console.log('New user %d', user._id);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function connectToDb() {
+  mongoose.connect(
+    `mongodb+srv://${dbConfig.username}:${dbConfig.password}@${dbConfig.clusterName}.oqca2.mongodb.net/${dbConfig.databaseName}?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+  );
+  
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'Connection error: '));
+  db.once('open', function() { console.log('Database successfully connected!') });
+}
+
+function bindAndStartServer() {
+  app.use('/api/', apiRouters)
+  app.listen(appConfig.port, () => console.log('Listening on port %d', appConfig.port))  
+}
+
+connectToDb();
+bindAndStartServer();
