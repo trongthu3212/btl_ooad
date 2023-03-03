924c2484581c6a1a157a20890217184a7f8d1522
Nguyễn Trung Hiếu <63243790+hieu404@users.noreply.github.com>
2023-03-03 23:15:40 +0700

    display question

---
+++ client/src/Components/Loader/Loader.jsx
@@ -0,0 +1,22 @@
+import "./Loader.scss";
+
+export default function Loader() {
+    return (
+        <div className="loader-container">
+            <div class="lds-spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
