7f77086604fff822940b6d5f861ac46b0a5c7913
pent0 <l51492003231@163.com>
2023-05-19 22:02:45 +0700

    add unaccept answer and remove search filter

--- server/src/controllers/AnswerController.js
+++ server/src/controllers/AnswerController.js
@@ -44,27 +44,38 @@ async function acceptAnswer(req, res) {
         return
     }
 
-    await answerModel.find({ post: answer.post, accepted: true }).then(
-        async answers => {
-            if (answers.length > 0) {
-                if ((answers.length == 1) && (answers[0]._id == answerId)) {
-                    res.sendStatus(200);
-                }
-                else {
-                    res.status(400).json({ error: "This post already has an accepted answer!" })
-                }
+    await answerModel.updateMany({ post: answer.post }, { accepted: false }).catch(err => res.status(500).json({ error: err }));
+    await answerModel.updateOne({ _id: answerId }, { accepted: true }).then(answer => res.sendStatus(200)).catch(err => res.status(500).json({ error: err }))
+}
 
-                return
-            }
+async function unacceptAnswer(req, res) {
+    let answerId = req.params.id
+    let answer = await answerModel.findById(answerId)
+        .catch(err => { res.status(500).json({ error: err })})
 
-            await answerModel.updateOne({ _id: answerId }, { accepted: true }).then(
-                answer => res.sendStatus(200)
-            ).catch(err => res.status(500).json({ error: err }))
-        }
-    ).catch(err => res.status(500).json({ error: err }))
+    if (answer == null) {
+        res.status(404).json({ error: "Answer ID not found!" })
+    }
+
+    let post = await postModel.findById(answer.post)
+    if (post == null) {
+        res.status(404).json({ error: "Can't find the post related to this answer!" })
+        return
+    }
+
+    console.log(post.author)
+    console.log(req.user._id)
+
+    if (!post.author.equals(req.user._id)) {
+        res.status(401).json({ error: "The logged in user is not the author of the question!" })
+        return
+    }
+
+    await answerModel.updateOne({ _id: answerId }, { accepted: false }).then(answer => res.sendStatus(200)).catch(err => res.status(500).json({ error: err }))
 }
 
 module.exports = {
     addAnswer: addAnswer,
-    acceptAnswer: acceptAnswer
+    acceptAnswer: acceptAnswer,
+    unacceptAnswer: unacceptAnswer
 }
